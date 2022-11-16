283163c7d4db039c0514520e9533a8abee952426
andrija-tosic <andonnet@gmail.com>
2022-11-16 20:42:51 +0100

    rename: moves set

--- game.py
+++ game.py
@@ -12,23 +12,23 @@ class Game:
     def __init__(self, n: int = 8, m: int = 8) -> None:
         self.n: int = n  # broj vrsta
         self.m: int = m  # broj kolona
-        self.first_players_turn: bool = True
+        self.v_players_turn: bool = True
 
-        self.player1_moves_left: set[Tuple[int, int]] = set()
-        self.player2_moves_left: set[Tuple[int, int]] = set()
+        self.v_possible_moves: set[Tuple[int, int]] = set()
+        self.h_possible_moves: set[Tuple[int, int]] = set()
 
         for i in range(n-2):
             for j in range(m-2):
-                self.player1_moves_left.add((i, j))
-                self.player2_moves_left.add((i, j))
+                self.v_possible_moves.add((i, j))
+                self.h_possible_moves.add((i, j))
 
         # vertikalni u poslednjoj koloni
         for i in range(n-1):
-            self.player1_moves_left.add((i, m-1))
+            self.v_possible_moves.add((i, m-1))
 
         # horizontalni u poslednjoj vrsti
         for i in range(m-1):
-            self.player2_moves_left.add((n-1, i))
+            self.h_possible_moves.add((n-1, i))
 
         self.board_matrix = [[Field.EMPTY for _ in range(m)] for _ in range(n)]
 
@@ -47,57 +47,57 @@ class Game:
         if not self.valid_move(x, y):
             return
 
-        fieldToWrite: Field = Field.PLAYER1 if self.first_players_turn else Field.PLAYER2
+        fieldToWrite: Field = Field.PLAYER1 if self.v_players_turn else Field.PLAYER2
 
         self.board_matrix[x][y] = fieldToWrite
 
-        self.player1_moves_left.discard((x, y))
-        self.player1_moves_left.discard((x, y-1))
+        self.v_possible_moves.discard((x, y))
+        self.v_possible_moves.discard((x, y-1))
 
-        self.player2_moves_left.discard((x, y))
-        self.player2_moves_left.discard((x-1, y))
+        self.h_possible_moves.discard((x, y))
+        self.h_possible_moves.discard((x-1, y))
 
-        if self.first_players_turn:  # vertical zauzima mesta (x, y) i (x, y+1)
+        if self.v_players_turn:  # vertical zauzima mesta (x, y) i (x, y+1)
             self.board_matrix[x][y+1] = fieldToWrite
 
             # vertikali sam sebi blokira jos jedno polje
             # # b #
             # #[b]#
             # #[b]#
-            self.player1_moves_left.discard((x, y+1))
+            self.v_possible_moves.discard((x, y+1))
 
             # horizontalnom blokira 2 polja
             # # # #
             # b[b]#
             # b[b]#
-            self.player2_moves_left.discard((x-1, y+1))
-            self.player2_moves_left.discard((x, y+1))
+            self.h_possible_moves.discard((x-1, y+1))
+            self.h_possible_moves.discard((x, y+1))
 
         else:  # horizontal zauzima mesta (x, y) i (x+1, y)
             self.board_matrix[x+1][y] = fieldToWrite
 
             # vertikalnom blokira jos 2 polja (polja iznad)
-            self.player1_moves_left.discard((x+1, y))
-            self.player1_moves_left.discard((x+1, y-1))
+            self.v_possible_moves.discard((x+1, y))
+            self.v_possible_moves.discard((x+1, y-1))
 
             # horizontalni samom sebi blokira jos jedno polje
-            self.player2_moves_left.discard((x+1, y))
+            self.h_possible_moves.discard((x+1, y))
 
-        self.first_players_turn = not self.first_players_turn
+        self.v_players_turn = not self.v_players_turn
         return
 
     def valid_move(self, x: int, y: int) -> bool:
-        if self.first_players_turn:
-            return (x, y) in self.player1_moves_left
+        if self.v_players_turn:
+            return (x, y) in self.v_possible_moves
         else:
-            return (x, y) in self.player2_moves_left
+            return (x, y) in self.h_possible_moves
 
     def evaluate(self) -> float:
         return 0
 
     # mora da bude O(1)
     def game_over(self) -> bool:
-        return len(self.player1_moves_left) == 0 or len(self.player2_moves_left) == 0
+        return len(self.v_possible_moves) == 0 or len(self.h_possible_moves) == 0
 
     def __str__(self):
         output_str = ""
