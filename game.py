49a6eb03f10f8e514740055e1c46e4f27b108ce4
stefan-foo <stojadinovicstefan59@gmail.com>
2022-11-22 16:50:37 +0100

    funkcija za proveru kraja igre

--- game.py
+++ game.py
@@ -44,6 +44,24 @@ def is_valid_move(state: State, move: tuple[int, str]) -> bool:
         return x + 1 < n and y < m and board[x + 1][y] is Square.EMPTY
 
 
+def is_game_over(state: State) -> bool:
+    board, to_move = state
+    n = len(board)
+    m = len(board[0])
+
+    if to_move is Player.VERTICAL:
+        for i in range(n - 1):
+            for j in range(m):
+                if board[i][j] is Square.EMPTY and board[i+1][j] is Square.EMPTY:
+                    return False
+    else:
+        for i in range(n):
+            for j in range(m - 1):
+                if board[i][j] is Square.EMPTY and board[i][j + 1] is Square.EMPTY:
+                    return False
+    return True
+
+
 def derive_state(state: State, move: tuple[int, str]) -> State:
     if not is_valid_move(state, move):
         raise Exception("Invalid move")
@@ -52,11 +70,9 @@ def derive_state(state: State, move: tuple[int, str]) -> State:
     new_board = [row[:] for row in board]
 
     if to_move is Player.HORIZONTAL:
-        new_board[x][y] = Square.HORIZONTAL
-        new_board[x][y + 1] = Square.HORIZONTAL
+        new_board[x][y] = new_board[x][y+1] = Square.HORIZONTAL
     else:
-        new_board[x][y] = Square.VERTICAL
-        new_board[x + 1][y] = Square.VERTICAL
+        new_board[x][y] = new_board[x + 1][y] = Square.VERTICAL
 
     new_to_move = Player.HORIZONTAL if to_move is Player.VERTICAL else Player.VERTICAL
     return State(board=new_board, to_move=new_to_move)
