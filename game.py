7c935a41a29bd05ddcc00a0347bf068b63260ae8
stefan-foo <stojadinovicstefan59@gmail.com>
2022-11-23 09:56:44 +0100

    drugaciji ispis poruka

--- game.py
+++ game.py
@@ -92,7 +92,7 @@ def print_state(state: State) -> None:
     output_arr = []
 
     output_arr.append("   A")
-    output_arr.extend([f"  {chr(65 + x) }" for x in range(1, m)])
+    output_arr.extend([f"  {chr(65 + x)}" for x in range(1, m)])
     output_arr.append("\n")
     for i in range(n):
         output_arr.append(f"{i + 1} ")
@@ -102,14 +102,18 @@ def print_state(state: State) -> None:
     print(str.join("", output_arr))
 
 
-game_state: State = get_initial_state(4, 4)
+game_state: State = get_initial_state(8, 8)
 while not is_game_over(game_state):
+    print_state(game_state)
     move = input(
         f"[{'VERTICAL' if game_state.to_move is Player.VERTICAL else 'HORIZONTAL'}] enter move: ")
     move = parse_move(move)
     if move:
-        game_state = derive_state(game_state, move) or game_state
-        print_state(game_state)
+        new_game_state = derive_state(game_state, move)
+        if new_game_state:
+            game_state = new_game_state
+        else:
+            print("Invalid move")
     else:
-        print("Invalid move")
+        print("Enter move in format ROW COLUMN")
 print(f"{'VERTICAL' if game_state.to_move is Player.HORIZONTAL else 'HORIZONTAL'} WON")
