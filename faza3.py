74d03c7ab8f040e7af757f940c761e0e9a01b694
stefan-foo <stojadinovicstefan59@gmail.com>
2022-12-27 00:04:13 +0100

    izbor igraca

--- faza3.py
+++ faza3.py
@@ -36,12 +36,12 @@ def evaluate_state(state: State) -> int:
 
 
 # @lru_cache(maxsize=256)
-def alfabeta(state: State, depth: int, alpha: float, beta: float) -> tuple[Move | None, int]:
+def alfabeta(state: State, depth: int, alpha: float, beta: float) -> tuple[Move, int]:
     if depth == 0 or is_game_over(state):
         return ((-1, -1), evaluate_state(state))
 
     if state.to_move is Turn.VERTICAL:
-        best_move = (None, -1001)
+        best_move = ((-1, -1), -1001)
         for move in state.v_possible_moves:
             child_state = derive_state(state, move)
             if (child_state):
@@ -52,7 +52,7 @@ def alfabeta(state: State, depth: int, alpha: float, beta: float) -> tuple[Move 
                 if alpha >= beta:
                     break
     else:
-        best_move = (None, 1001)
+        best_move = ((-1, -1), 1001)
         for move in state.h_possible_moves:
             child_state = derive_state(state, move)
             if (child_state):
@@ -67,66 +67,26 @@ def alfabeta(state: State, depth: int, alpha: float, beta: float) -> tuple[Move 
 
 DEPTH_TEST = 4
 
-mode = PlayingMode.PLAYER_VS_PLAYER
 
+def game_loop(n, m, player1, player2, initial_to_move) -> None:
+    game_state = create_initial_state(n, m, initial_to_move)
 
-def human_turn(game_state):
-    move = input_move(game_state.to_move)
-    if move:
-        new_game_state = derive_state(game_state, move)
-        if new_game_state:
-            game_state = new_game_state
-        else:
-            print("Invalid move")
-    else:
-        print("Enter move in format ROW COLUMN")
-    return game_state, move
-
-
-def game_loop() -> None:
-    if mode is PlayingMode.PLAYER_VS_AI or mode is PlayingMode.PLAYER_VS_PLAYER:
-        n, m = input_board_dimensions()
-    else:
-        n, m = 10, 10
-
-    game_state: State = create_initial_state(n, m)
-
-    last_move = None
-    move = None
-    eval = 0
+    to_move, next_to_move = player1, player2
     while not is_game_over(game_state):
-        print_state(game_state)
-        if last_move:
-            print(
-                f"\nMove played: {(last_move[0] + 1, chr(ord('A') + last_move[1]))} [{eval}]\n")
-        if mode is PlayingMode.AI_VS_AI:
-            move, eval = alfabeta(
-                game_state, DEPTH_TEST, -math.inf, math.inf)
-        elif mode is PlayingMode.PLAYER_VS_AI:
-            if game_state.to_move is Turn.VERTICAL:  # Human turn
-                game_state, move = human_turn(game_state)
-            else:  # AI turn
-                move, eval = alfabeta(
-                    game_state, DEPTH_TEST, -math.inf, math.inf)
-        elif mode is PlayingMode.PLAYER_VS_PLAYER:
-            game_state, move = human_turn(game_state)
-
-        # ovaj deo ispod da se ocisti, nzm sta je to kog djavola
-        last_move = move
-        if move:
-            new_game_state = derive_state(game_state, move)
+        if to_move == Player.AI:
+            move, eval = alfabeta(game_state, DEPTH_TEST, -math.inf, math.inf)
         else:
-            break
-        if new_game_state is not None:
-            game_state = new_game_state
+            move = input_valid_move(game_state)
 
-    # isto i ovo ispod
-    last_state = game_state
-    alfabeta(last_state, DEPTH_TEST, -math.inf, math.inf)
+        new_state = derive_state(game_state, move)
+        if new_state:
+            game_state = new_state
+
+        to_move, next_to_move = next_to_move, to_move
+        print_state(game_state)
+
     print_state(game_state)
-    print(
-        f"{'VERTICAL' if game_state.to_move is Turn.HORIZONTAL else 'HORIZONTAL'} WON")
 
 
 if __name__ == "__main__":
-    game_loop()
+    game_loop(8, 8, Player.AI, Player.AI, Turn.VERTICAL)
