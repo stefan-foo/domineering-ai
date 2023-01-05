c5385c3ab672a12141ac2a5d4e5d18a526996467
stefan-foo <stojadinovicstefan59@gmail.com>
2023-01-05 01:19:40 +0100

    tt test

--- faza3.py
+++ faza3.py
@@ -2,6 +2,7 @@ import math
 from faza1 import *
 from faza2 import *
 from time import time
+from tt import *
 
 
 def derive_isolated_moves(state: State) -> tuple[set[Move], set[Move]]:
@@ -30,41 +31,57 @@ def evaluate_state(state: State) -> int:
     return 2*len(state.v_possible_moves) + 3*len(v_safe_moves) - (2*len(state.h_possible_moves) + 3*len(h_safe_moves))
 
 
-def alfabeta(state: State, depth: int, alpha: float, beta: float) -> tuple[Move, int]:
+tt_cutoff = 0
+
+
+def alfabeta(state: State, depth: int, alpha: float, beta: float, tt: TranspositionTable) -> tuple[Move, int]:
     if depth == 0 or is_game_over(state):
         value = evaluate_state(state)
         return ((-1, -1), value)
 
+    # global tt_cutoff
+
+    # tt_val = tt.retrieve(state.board)
+    # if tt_val is not None:
+    #     tt_move, tt_depth = tt_val
+    #     if depth <= tt_depth:
+    #         tt_cutoff += 1
+    #         return tt_move
+
     if state.to_move is Turn.VERTICAL:
         best_move = ((-1, -1), -1001)
         for move in set(state.v_possible_moves):
-            child_state = modify_state(state, move)
+            # child_state = modify_state(state, move)
+            child_state = derive_state(state, move)
             if (child_state):
-                candidate = alfabeta(child_state, depth - 1, alpha, beta)
+                candidate = alfabeta(child_state, depth - 1, alpha, beta, tt)
                 if candidate[1] > best_move[1]:
                     best_move = (move, candidate[1])
                 alpha = max(alpha, best_move[1])
                 if alpha >= beta:
-                    undo_move(state, move)
+                    # undo_move(state, move)
                     break
-            undo_move(state, move)
+            # undo_move(state, move)
     else:
         best_move = ((-1, -1), 1001)
         for move in set(state.h_possible_moves):
-            child_state = modify_state(state, move)
+            # child_state = modify_state(state, move)
+            child_state = derive_state(state, move)
             if (child_state):
-                (candidate) = alfabeta(child_state, depth - 1, alpha, beta)
+                (candidate) = alfabeta(child_state, depth - 1, alpha, beta, tt)
                 if candidate[1] < best_move[1]:
                     best_move = (move, candidate[1])
                 beta = min(beta, best_move[1])
                 if alpha >= beta:
-                    undo_move(state, move)
+                    # undo_move(state, move)
                     break
-            undo_move(state, move)
+            # undo_move(state, move)
+
+    # tt.store(state.board, (best_move), depth)
     return best_move
 
 
-MIN_DEPTH = 3
+MIN_DEPTH = 4
 
 
 def dynamic_depth(state: State) -> int:
@@ -77,6 +94,7 @@ move_duration_list = []
 
 
 def game_loop(n: int, m: int, player1: Player, player2: Player, first_to_move: Turn) -> None:
+    tt = TranspositionTable(n, m)
     game_state = create_initial_state(n, m, first_to_move)
 
     to_move, next_to_move = player1, player2
@@ -89,7 +107,8 @@ def game_loop(n: int, m: int, player1: Player, player2: Player, first_to_move: T
             print(depth)
 
             start_time = time()
-            move, _ = alfabeta(game_state, depth, -math.inf, math.inf)
+            move, _ = alfabeta(game_state, depth, -math.inf,
+                               math.inf, tt)
 
             move_duration_list.append((move_number, time() - start_time))
         else:
@@ -103,9 +122,11 @@ def game_loop(n: int, m: int, player1: Player, player2: Player, first_to_move: T
 
         print_state(game_state)
 
+    print("Hits: ", tt.hits, " Misses: ", tt.misses, " Used: ", tt_cutoff)
+
 
 if __name__ == "__main__":
-    game_loop(8, 8, Player.AI, Player.AI, Turn.VERTICAL)
+    game_loop(8, 8, Player.AI, Player.HUMAN, Turn.VERTICAL)
 
     with open(f"moves_duration_8x8_depth_{MIN_DEPTH}_two_sets_board_undo_move.txt", "w") as f:
         for t in move_duration_list:
---
+++ tt.py
@@ -0,0 +1,31 @@
+import random
+import sys
+
+
+class TranspositionTable:
+    def __init__(self, n, m):
+        self.table = {}
+        self.hits = 0
+        self.key = [[[random.randint(0, sys.maxsize) for _ in range(
+            m)] for _ in range(n)] for _ in range(2)]
+        self.misses = 0
+
+    def store(self, board, best_move, depth):
+        self.table[self.cache_key(board)] = best_move, depth
+
+    def retrieve(self, board):
+        return None
+        key = self.cache_key(board)
+        if key in self.table:
+            self.hits += 1
+            return self.table[key]
+        self.misses += 1
+        return None
+
+    def cache_key(self, board) -> int:
+        hash = 0
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if (board[i][j] != 0):
+                    hash = hash ^ self.key[board[i][j] - 1][i][j]
+        return hash
